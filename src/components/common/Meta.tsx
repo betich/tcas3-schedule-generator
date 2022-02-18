76071cbffb648ed69b5940fa895214bf8efb155e
betich <betich365@gmail.com>
2022-02-19 04:38:01 +0700

    add open graph tags

--- src/components/common/Meta.tsx
+++ src/components/common/Meta.tsx
@@ -1,11 +1,23 @@
-import Head from "next/head";
-import { FC } from "react";
+import Head from "next/head"
+import { FC } from "react"
 
 export const MetaData: FC = () => {
   return (
     <Head>
-      <title>TCAS 3 Exam Schedule Generator</title>
+      <title>TCAS Exam Schedule Generator</title>
       <meta name="description" content="ระบบสร้างตารางสอบ TCAS รอบ 3 ปีการศึกษา 2565" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://tcas-schedule.betich.me/" />
+      <meta property="og:title" content="TCAS Exam Schedule Generator" />
+      <meta property="og:description" content="ระบบสร้างตารางสอบ TCAS รอบ 3 ปีการศึกษา 2565" />
+      <meta property="og:image" content="/preview.jpg" />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://tcas-schedule.betich.me/" />
+      <meta property="twitter:title" content="TCAS Exam Schedule Generator" />
+      <meta property="twitter:description" content="ระบบสร้างตารางสอบ TCAS รอบ 3 ปีการศึกษา 2565" />
+      <meta property="twitter:image" content="/preview.jpg" />
     </Head>
-  );
-};
+  )
+}
